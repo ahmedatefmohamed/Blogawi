4d7c3baf78de386077657d890f054641fb5b2c32
ahmedatefmohamed <43454769+ahmedatefmohamed@users.noreply.github.com>
2021-01-25 01:35:18 +0200

    Update Users.js

--- models/Users.js
+++ models/Users.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
-const { Schema } = mongoose;
+// const { Schema } = mongoose;
+const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
     username: {
