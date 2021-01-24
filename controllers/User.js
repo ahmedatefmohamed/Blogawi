d2f1758fcb1995e538864ea5ca3a47cf3683e245
ahmedatefmohamed <43454769+ahmedatefmohamed@users.noreply.github.com>
2021-01-25 00:38:54 +0200

    init

--- controllers/User.js
+++ controllers/User.js
@@ -1,7 +1,8 @@
 const jwt = require('jsonwebtoken');
 const { promisify } = require('util');
-const asyncSign = promisify(jwt.sign);
+const signJWT = promisify(jwt.sign);
 const User = require('../models/Users');
+const CustomError = require('../helpers/CustomError')
 
 const register = (user) => User.create(user);
 const login = ({ username, password }) => {
@@ -14,7 +15,7 @@ const login = ({ username, password }) => {
         //throw Error("UN_AUTHENTICATED");
         throw (new CustomError(401, 'AUTHENTICATION_REQUIRED', 'User name or password is incorrect'));
     }
-    const token = await asyncSign({ username: user.username, id: user.id },
+    const token = await signJWT({ username: user.username, id: user.id },
          'SECRET_MUST_BE_COMPLEX', { expiresIn: '1d' });
       return { ...user.toJSON(), token };
       // match input password with user data using bcrypt
