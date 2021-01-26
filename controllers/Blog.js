6bce1ea791b2060809041a96e74ee6ce9a129e54
ahmedatefmohamed <43454769+ahmedatefmohamed@users.noreply.github.com>
2021-01-26 03:27:27 +0200

    init

--- controllers/Blog.js
+++ controllers/Blog.js
@@ -3,7 +3,7 @@ const Blog = require('../models/Blogs');
 const create = (blog) => Blog.create(blog);
 const getAll = (query) => Blog.find(query).exec();
 const getById = (id) => Blog.findById(id).exec();
-const getByTitle= ({title}) => Blog.find(title).exec();
+const getByTitle= ({title}) => Blog.find({title}).exec();
 const getByTag = ({tag}) => Blog.find({tag}).exec();
 const getByAuthor= ({author}) => Blog.find({author}).exec();
 const getNew = (query) => Blog.find(query).sort([['createdAT', -1]]).exec();
